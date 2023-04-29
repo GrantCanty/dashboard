e7f01f9ae80a07c9d0aad6a498a06ab3ccf12c8c
GrantCanty <gc933815@ohio.edu>
2023-04-29 01:42:30 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/types/types.tsx
@@ -0,0 +1,23 @@
+class Component {
+
+}
+
+class WeatherView extends Component {
+
+}
+
+class SportsView extends Component {
+
+}
+
+class FinanceView extends Component {
+
+}
+
+class NewsView extends Component {
+
+}
+
+class CalendarView extends Component {
+    
+}
