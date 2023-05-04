863cdc3f6f0741f731019d311fd49ccaa68cdca5
GrantCanty <gc933815@ohio.edu>
2023-05-04 00:19:02 -0600

    added conditional to modal to not display when showModal is false

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -2,7 +2,16 @@ import { FC } from "react";
 import ReactDOM from "react-dom";
 import "../assets/styles/modal.css"
 
-const Modal: FC =() => {
+interface Props {
+    showModal: boolean;
+    setShow: (value: boolean) => void;
+}
+
+const Modal: FC<Props> =({showModal, setShow}: Props) => {
+
+    if(!showModal) {
+        return null
+    }
 
     return ReactDOM.createPortal(
         <div className="modal-bg">
